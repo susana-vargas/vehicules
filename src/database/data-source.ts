d4461ce33e9f8c181f7b3706046434eaba0b28ee
Susana <hernandeznerisusanaisabel@gmail.com>
2024-04-01 18:09:27 -0600

    the configuration of the database module and all that goes with it as well as the installation of typeorm pg was carried out

---
+++ src/database/data-source.ts
@@ -0,0 +1,10 @@
+import { DataSourceOptions } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'root',
+  password: '123456',
+  database: 'vehicule_api',
+};
--- src/vehicules/vehicule.module.ts
+++ src/vehicules/vehicule.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 
 import { VehiculeController } from "./vehicule.controller";
 import { VehiculeService } from "./vehicule.service";
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
   controllers: [VehiculeController],
   providers: [VehiculeService],
+  imports: [DatabaseModule],
 })
 export class VehiculeModule {}
